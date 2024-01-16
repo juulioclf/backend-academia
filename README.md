8b013fcecd1cb4c7255b39127f3daa80e54e5ad9
Julio Ferreira <juliolima731@gmail.com>
2024-01-16 18:45:23 -0400

    test: adding routes to test

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 meta: 
--dar um ping no banco de dados através de uma rota
+-dar um ping no através de uma rota
 
 como:
 - por meio do python com framework flask, irei conectar o banco de dados e fornecer alguma coisa em uma rota
+
+feito:
+- apenas ping em uma rota de testes
+
+não feito:
+- configuração do banco, conexão com banco e leitura do banco
+
+16/01/2024
 
 ______________________________________________
 
@@ -48,5 +56,5 @@ commands:
 commands for run:
 
 ```
-    flask --app <nome do app> run
+    flask --app app run
 ```
--- app.py
+++ app.py
@@ -5,3 +5,36 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello JULIN, become a friend!</p>"
+
+
+@app.route("/usuarios")
+def get_users():
+    usuarios = [  
+        {
+            "name": "julio ferreira",
+            "email": "julio@gmail.com"
+        },
+        {
+            "name": "miguel angelo",
+            "email": "miguel@gmail.com"
+        }
+    ]
+    return usuarios
+
+@app.route("/usuarios/1")
+def get_users_by_id():
+    usuario =  {
+            "name": "julio ferreira",
+            "email": "julio@gmail.com",
+            "apelido": "badmilk",
+            "linguagem": "python"
+        }
+    return usuario
+
+@app.route("/usuarios")
+def get_users_by_id():
+    params = {
+        name: "params.name",
+        email: "params.email"
+    }
+    return usuario
